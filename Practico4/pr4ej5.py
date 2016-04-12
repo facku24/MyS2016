a72a7e6c611ea0757267408b6d733a93b7d2bef6
Guest <guest-EzwqLb@lab08.famaf.unc.edu.ar>
2016-04-12 12:59:37 -0300

    siguiendo modificando el ejercicio 5

--- Practico4/pr4ej5.py
+++ Practico4/pr4ej5.py
@@ -1,6 +1,19 @@
 from random import random
 import math
 
-def gen_poisson():
+def gen_poisson(param):
+	U = random()
+	i = 0
+	p = math.e**(-param)
+	F = p
+	while (U >= F):
+		p = (param * p)/float(i+1)
+		F += p
+		i += 1
 	
-def metodo_acept_rech():
+	return i
+
+def metodo_acept_rech(k):
+	poisson =  gen_poisson()
+	while
+	if (poisson <= K)
