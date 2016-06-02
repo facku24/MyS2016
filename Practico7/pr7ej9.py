d0b8081186769c46495fb57bd5246dbd319d24e2
facku24 <facku24@gmail.com>
2016-06-02 12:42:46 -0300

    Empezando el ejercicio8

---
+++ Practico7/pr7ej9.py
@@ -0,0 +1,13 @@
+import math
+from random import random
+from scipy.stats.distributions import chi2
+
+def rango_R():
+	muestra1 = [65.2,67.1,69.4,78.4,74.0,80.3]
+	muestra2 = [59.4,72.1,68.0,66.2,58.5]
+	muestra_total = muestra1 + muestra2
+	muestra2.sort()
+	muestra_total.sort()
+	
+	while(i < len(muestra2))
+		
