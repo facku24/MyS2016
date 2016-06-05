d8a5359f076639cd17dd891f2e8030ff9f85f058
Facku24 <facku24@gmail.com>
2016-06-05 14:17:25 -0300

    Casi terminando el practico. Falta chequear que los resultados sean exactos

--- Practico7/ej9f.py
+++ Practico7/ej9f.py
@@ -1,25 +1,40 @@
 
-
 muestra = [342, 361, 448, 453, 504]
 simulacion = [186, 220, 225, 456, 276, 199, 371, 426, 242, 311]
 
-simulacion.sort()
+muestra1 = [2,3,4]
+muestra2 = [3,5,7]
 
-def rango(muestra, simulacion):
+def rango(totalset, subset):
+	'''
+	This function generates the rank fot subset.
+
+	param - totalset: Debe de ser el conjunto total y ordenado de todas las muestras
+					  simuladas y las muestras observadas
+	param - subset: Debe de ser el subconjunto al cual le quiero generar el rango
+	'''
 	rango = 0
 	index = 0
 
-	muestra_total = muestra + simulacion
-	muestra_total.sort()
-
-	for elem in muestra:
+	totalset.sort()
+	subset.sort()
+	
+	for elem in subset:
 		while True:
-			if (muestra_total[index] == elem):
-				rango += index + 1
+			if (totalset[index] == elem):
+				if (index < len(totalset)-1 and totalset[index] == totalset[index+1]):
+					rango += (2*index+3) /float(2)
+				elif (index > 0 and totalset[index] == totalset[index-1]):
+					rango += (2*index+1) /float(2)
+				else:
+					rango += index + 1
+					index += 1
 				break
 			else:
 				index+=1
 	return rango
 
-print(rango(simulacion, muestra))
-print(rango(muestra, simulacion))
+#print(rango(simulacion+muestra, muestra))
+#print(rango(simulacion+muestra, simulacion))
+print(rango(muestra1+muestra2, muestra1))
+print(rango(muestra1+muestra2, muestra2))
